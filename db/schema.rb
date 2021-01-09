81cb0b48707edbb5e28a0bd50b1671e80d71daab
naoyasakuma <4869ns@gmail.com>
2021-01-09 21:08:22 +0900

    [update]usersテーブルにカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,11 @@ ActiveRecord::Schema.define(version: 2021_01_09_114215) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.text "introduction"
+    t.string "icon"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
