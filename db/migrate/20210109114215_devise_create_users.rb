09d16b7f4f03989e7deb3a56d839652ffbc7da46
naoyasakuma <4869ns@gmail.com>
2021-01-10 18:29:39 +0900

    [change]usersテーブルのiconカラムをtext型に変更

--- db/migrate/20210109114215_devise_create_users.rb
+++ db/migrate/20210109114215_devise_create_users.rb
@@ -8,7 +8,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.text :introduction
-      t.string :icon
+      t.text :icon
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2021_01_09_114215) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.text "introduction"
-    t.string "icon"
+    t.text "icon"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
