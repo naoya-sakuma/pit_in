39f5b81cbe1568cecd42f28e42b10c367b4c789a
naoyasakuma <4869ns@gmail.com>
2021-01-12 12:55:45 +0900

    [add]アカウント登録時、確認用メール送信を通知する画面を表示するテストを作成

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -12,40 +12,5 @@ RSpec.describe 'ユーザー登録機能', type: :system do
         expect(page).to have_content '本人確認用のメールを送信しました。'
       end
     end
-    context '必要情報を入力し、アカウントを登録した場合' do
-      it '本人確認用のメールが送信される' do
-
-      end
-    end
-    context '本人確認メールで本人確認が完了した場合' do
-      it 'ユーザー情報が登録される' do
-
-      end
-    end
-    context 'メールアドレスを入力せずに登録した場合' do
-      it 'エラーが発生し、メールアドレスを入力してくださいと表示される' do
-
-      end
-    end
-    context 'パスワードを入力せずに登録した場合' do
-      it 'エラーが発生し、パスワードを入力してくださいと表示される' do
-
-      end
-    end
-  end
-  describe 'Googleアカウントログイン機能' do
-    context '' do
-      it '' do
-
-      end
-    end
-  end
-
-  describe 'セッション機能' do
-    context 'ユーザーがログインした場合' do
-      it 'ログイン状態になる' do
-
-      end
-    end
   end
 end
