06c653ee239a6b55d1607e26245887621127e5f4
naoyasakuma <4869ns@gmail.com>
2021-01-12 16:37:48 +0900

    [clean]user_specをsignupとloginに分割

---
+++ spec/system/login_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+RSpec.describe 'ユーザー登録機能', type: :system do
+  describe 'Googleアカウントログイン機能' do
+    context '' do
+      it '' do
+
+      end
+    end
+  end
+
+  describe 'セッション機能' do
+    context 'ユーザーがログインした場合' do
+      it 'ログイン状態になる' do
+
+      end
+    end
+  end
+end
--- spec/system/sign_up_spec.rb
+++ spec/system/sign_up_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 RSpec.describe 'ユーザー登録機能', type: :system do
+  before do
+    ActionMailer::Base.deliveries.clear
+  end
+
   describe 'メールアドレスによるユーザー新規登録' do
     context '必要情報を入力し、アカウントを登録した場合' do
       it '本人確認用のメールを送信しました。と表示される' do
-        visit new_user_registration_path
+        visit root_path
         fill_in '名前', with: 'test'
         fill_in 'メールアドレス', with: 'test@test.com'
         fill_in 'パスワード', with: 'test@test.com'
@@ -14,17 +18,17 @@ RSpec.describe 'ユーザー登録機能', type: :system do
     end
     context '必要情報を入力し、アカウントを登録した場合' do
       it '本人確認用のメールが送信される' do
-
-      end
-    end
-    context '本人確認メールで本人確認が完了した場合' do
-      it 'ユーザー情報が登録される' do
-
+        visit root_path
+        fill_in '名前', with: 'test'
+        fill_in 'メールアドレス', with: 'test@test.com'
+        fill_in 'パスワード', with: 'test@test.com'
+        fill_in '確認用パスワード', with: 'test@test.com'
+        expect { click_button 'アカウント登録' }.to change { ActionMailer::Base.deliveries.size }.by(1)
       end
     end
     context 'メールアドレスを入力せずに登録した場合' do
       it 'エラーが発生し、メールアドレスを入力してくださいと表示される' do
-        visit new_user_registration_path
+        visit root_path
         fill_in '名前', with: 'test'
         fill_in 'パスワード', with: 'test@test.com'
         fill_in '確認用パスワード', with: 'test@test.com'
@@ -34,26 +38,15 @@ RSpec.describe 'ユーザー登録機能', type: :system do
     end
     context 'パスワードを入力せずに登録した場合' do
       it 'エラーが発生し、パスワードを入力してくださいと表示される' do
-        visit new_user_registration_path
+        visit root_path
         fill_in '名前', with: 'test'
         fill_in 'メールアドレス', with: 'test@test.com'
         click_on 'アカウント登録'
         expect(page).to have_content 'パスワードを入力してください'
       end
     end
-  end
-
-  describe 'Googleアカウントログイン機能' do
-    context '' do
-      it '' do
-
-      end
-    end
-  end
-
-  describe 'セッション機能' do
-    context 'ユーザーがログインした場合' do
-      it 'ログイン状態になる' do
+    context '本人確認メールで本人確認が完了した場合' do
+      it 'ユーザー情報が登録される' do
 
       end
     end
