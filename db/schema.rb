6aa1e0ba41338569785af56347f6fd9752666490
naoyasakuma <4869ns@gmail.com>
2021-01-13 17:57:52 +0900

    [add]goalsテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_11_035543) do
+ActiveRecord::Schema.define(version: 2021_01_13_085018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "goals", force: :cascade do |t|
+    t.string "title"
+    t.date "term"
+    t.string "purpose"
+    t.string "status"
+    t.text "when_succeed"
+    t.text "when_fail"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
