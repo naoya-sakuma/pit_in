553571b19bdf9d0ec6c2deefaa30d38808196fc6
naoyasakuma <4869ns@gmail.com>
2021-01-18 12:54:55 +0900

    [update]solutionsテーブルとtasksテーブルの親子関係を設定

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -1,6 +1,7 @@
 class Solution < ApplicationRecord
   belongs_to :problem, inverse_of: :solutions
-  has_many :tasks
+  has_many :tasks, inverse_of: :solution, dependent: :destroy
+  accepts_nested_attributes_for :tasks, reject_if: proc { |attributes| attributes['title'].blank?}
 
   validates :title,  presence: true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
-  belongs_to :solution
+  belongs_to :solution, inverse_of: :tasks
   has_many :tasks
 
   validates :title,  presence: true
