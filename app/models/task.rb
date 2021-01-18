45a84e433150d19bbdf1219e3bce6fa5a77bb12c
naoyasakuma <4869ns@gmail.com>
2021-01-18 14:51:42 +0900

    [update]tasksテーブルとstepsテーブルの親子関係を設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,8 @@
 class Task < ApplicationRecord
   #belongs_to :user
   belongs_to :solution, inverse_of: :tasks
-  has_many :tasks
+  has_many :tasks, inverse_of: :task, dependent: :destroy
+  accepts_nested_attributes_for :steps, reject_if: proc { |attributes| attributes['title'].blank?}
 
   validates :title,  presence: true
 end
