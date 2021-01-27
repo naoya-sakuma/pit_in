303cd97f751cb0f30e6b233968ff1dc2459fa1b8
naoyasakuma <4869ns@gmail.com>
2021-01-27 16:58:57 +0900

    [update]weeklyプランで編集内容が保存されるように設定

--- app/controllers/weekly_plans_controller.rb
+++ app/controllers/weekly_plans_controller.rb
@@ -1,6 +1,6 @@
 class WeeklyPlansController < ApplicationController
   before_action :set_goal, only: [:edit, :update, :destroy]
-  
+
   def index
     @goals = current_user.goals.where(status: '取組中')
   end
@@ -10,7 +10,7 @@ class WeeklyPlansController < ApplicationController
 
   def update
     if @goal.update(update_goal_params)
-      redirect_to edit_monthly_plan_path, notice: '変更が保存されました'
+      redirect_to edit_weekly_plan_path, notice: '変更が保存されました'
     else
       render :edit
     end
@@ -20,7 +20,8 @@ class WeeklyPlansController < ApplicationController
   def update_goal_params
     params.require(:goal).permit(:_destroy, :id,
                                  problems_attributes: [:done, :_destroy, :id,
-                                 solutions_attributes:[:working, :done, :_destroy, :id]])
+                                 solutions_attributes:[:working, :done, :_destroy, :id,
+                                 tasks_attributes:[:working, :done, :_destroy, :id]]])
   end
 
   def set_goal
