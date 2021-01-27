9718abbc75afc05740ae6daefb20515cdffb0ffd
naoyasakuma <4869ns@gmail.com>
2021-01-27 16:04:30 +0900

    [change]タイトルなしでも値の更新できるようにreject_ifを削除

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,7 +1,8 @@
 class Goal < ApplicationRecord
   belongs_to :user
-  has_many :problems, inverse_of: :goal, dependent: :destroy
-  accepts_nested_attributes_for :problems, reject_if: proc { |attributes| attributes['title'].blank?}
-
+  #has_many :problems, inverse_of: :goal, dependent: :destroy
+  has_many :problems, dependent: :destroy
+  #accepts_nested_attributes_for :problems, reject_if: proc { |attributes| attributes['title'].blank?}
+  accepts_nested_attributes_for :problems
   validates :title,  presence: true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ApplicationRecord
   #belongs_to :user
   belongs_to :solution, inverse_of: :tasks
   has_many :steps, inverse_of: :task, dependent: :destroy
-  accepts_nested_attributes_for :steps, reject_if: proc { |attributes| attributes['title'].blank?}
+  accepts_nested_attributes_for :steps
 
   validates :title,  presence: true
 end
