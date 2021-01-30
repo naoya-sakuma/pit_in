08531a624c79704bd6d6b2a36b0242d2d086340e
naoyasakuma <4869ns@gmail.com>
2021-01-30 13:11:03 +0900

    [change]許可するパラメーターを変更

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -45,10 +45,10 @@ class GoalsController < ApplicationController
   private
   def goal_params
     params.require(:goal).permit(:title, :day_to_start, :day_to_finish, :purpose, :status, :when_succeed, :when_fail,
-                                 problems_attributes: [:title,
-                                 solutions_attributes:[:title,
-                                 tasks_attributes:    [:title,
-                                 steps_attributes:    [:title]]]])
+                                 problems_attributes: [:title, :_destroy, :id,
+                                 solutions_attributes:[:title, :_destroy, :id,
+                                 tasks_attributes:    [:title, :_destroy, :id,
+                                 steps_attributes:    [:title, :_destroy, :id]]]])
   end
 
   def update_goal_params
