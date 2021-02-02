adde7341292dec0e1eaf7f0339035f6db484a7a5
naoyasakuma <4869ns@gmail.com>
2021-02-02 14:41:26 +0900

    [update]マイページ、画像のサイズと位置を調整

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   storage :file
-  process :resize_to_fit => [175, 175]
+  process :resize_to_fill => [175, 175, "Center"]
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
