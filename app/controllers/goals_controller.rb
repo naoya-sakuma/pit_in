b9c48869498392812c08e443d6de64e1b687d56d
naoyasakuma <4869ns@gmail.com>
2021-02-04 11:29:31 +0900

    [clean]余計なコードを削除

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,10 +1,8 @@
 class GoalsController < ApplicationController
   before_action :set_goal, only: [:show, :edit, :update, :destroy]
 
-  PER = 4
-
   def index
-    @goals = current_user.goals.page(params[:page]).per(PER)
+    @goals = current_user.goals.page(params[:page]).per(4)
 
     respond_to do |format|
       format.html
