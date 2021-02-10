fcc9265064f1a229bcf41b402d33915caeadbaa5
naoyasakuma <4869ns@gmail.com>
2021-02-10 19:03:23 +0900

    [fix]目標作成時、問題のタイトルが空欄だと保存しないように設定

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,7 +1,17 @@
 class Goal < ApplicationRecord
   belongs_to :user
   has_many :problems, dependent: :destroy
-  accepts_nested_attributes_for :problems, allow_destroy: true
+  #accepts_nested_attributes_for :problems, allow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for :problems, allow_destroy: true, reject_if: :reject_create_problem
+
+  def reject_create_problem(problems_attributes)
+    title_empty = problems_attributes[:title].blank?
+    status_empty = problems_attributes[:status].blank?
+    problems_attributes.merge!(_destroy: 1) if title_empty && status_empty
+    !title_empty && status_empty
+  end
+
+
 
   validates :title,  presence: true
 
