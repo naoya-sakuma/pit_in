d298a17e7976df5e6bcf90ec73eaf4f6b1084152
naoyasakuma <4869ns@gmail.com>
2021-02-12 21:04:02 +0900

    [clean]現状使用していないメソッドをコメントアウト

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -4,9 +4,9 @@ class Goal < ApplicationRecord
   accepts_nested_attributes_for :problems, allow_destroy: true#, reject_if: :reject_create_problem
   validates :title,  presence: true
 
-  def reject_create_problem(problems_attributes)
-    title_empty = problems_attributes[:title].blank?
-    status_empty = problems_attributes[:status].blank?
-    problems_attributes.merge!(_destroy: '1') if title_empty && status_empty
-  end
+  # def reject_create_problem(problems_attributes)
+  #   title_empty = problems_attributes[:title].blank?
+  #   status_empty = problems_attributes[:status].blank?
+  #   problems_attributes.merge!(_destroy: '1') if title_empty && status_empty
+  # end
 end
