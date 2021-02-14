c7d2a05951788fbcf3a9ac3951698cf8f3937c12
naoyasakuma <4869ns@gmail.com>
2021-02-14 11:25:48 +0900

    [add]usersテーブルにnicknameカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_11_074721) do
+ActiveRecord::Schema.define(version: 2021_02_14_022430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,8 +108,10 @@ ActiveRecord::Schema.define(version: 2021_02_11_074721) do
     t.string "unconfirmed_email"
     t.string "provider", default: "", null: false
     t.string "uid", default: "", null: false
+    t.string "nickname"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["nickname"], name: "index_users_on_nickname", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
