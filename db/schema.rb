59edb9e774a67316594264e03663c091fa080638
naoyasakuma <4869ns@gmail.com>
2021-02-16 07:19:41 +0900

    [add]memberモデル、memberテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_121459) do
+ActiveRecord::Schema.define(version: 2021_02_15_221818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,13 @@ ActiveRecord::Schema.define(version: 2021_02_15_121459) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_isolated_steps_on_user_id"
+  end
+
+  create_table "members", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "community_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "problems", force: :cascade do |t|
---
+++ spec/factories/members.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :member do
+    user_id { 1 }
+    community_id { 1 }
+  end
+end
