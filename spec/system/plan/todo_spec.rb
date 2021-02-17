1e544a1af911db4b54ab65581483667c39afb37b
naoyasakuma <4869ns@gmail.com>
2021-02-18 00:28:34 +0900

    [clean]plan関連のテストコードを整理

--- spec/system/plan/todo_spec.rb
+++ spec/system/plan/todo_spec.rb
@@ -1,1 +1,34 @@
-# rspec spec/system/todo_spec.rb
+# bundle exec rspec spec/system/plan/todo_spec.rb
+
+require 'rails_helper'
+RSpec.describe '計画機能', type: :system do
+  describe 'ToDo' do
+    before do
+      user = FactoryBot.create(:user)
+      goal = FactoryBot.create(:goal, user: user)
+      problem = FactoryBot.create(:problem, goal: goal)
+      solution = FactoryBot.create(:solution, problem: problem)
+      task = FactoryBot.create(:task, solution: solution)
+      step = FactoryBot.create(:step, task: task)
+      step = FactoryBot.create(:second_step, task: task)
+      #user.skip_confirmation!
+      user.save!
+      visit new_user_session_path
+      fill_in 'メールアドレス', with: 'test@test.com'
+      fill_in 'パスワード', with: 'test@test.com'
+      click_button 'ログイン'
+    end
+    context '一覧ページを見た場合' do
+      it '取組中のステップが表示される' do
+        visit today_jobs_path
+        expect(page).to have_content "取組中のテストステップ"
+      end
+    end
+    context '一覧ページを見た場合' do
+      it '保留中のステップは表示されない' do
+        visit today_jobs_path
+        expect(page).not_to have_content "保留中のテストステップ"
+      end
+    end
+  end
+end
--- spec/system/plan/weekly_spec.rb
+++ spec/system/plan/weekly_spec.rb
@@ -1,1 +1,32 @@
-# rspec spec/system/weekly_spec.rb
+# bundle exec rspec spec/system/plan/weekly_spec.rb
+
+require 'rails_helper'
+RSpec.describe '計画機能', type: :system do
+  describe '今週の計画' do
+    before do
+      user = FactoryBot.create(:user)
+      goal = FactoryBot.create(:goal, user: user)
+      problem = FactoryBot.create(:problem, goal: goal)
+      solution = FactoryBot.create(:solution, problem: problem)
+      solution = FactoryBot.create(:second_solution, problem: problem)
+      #user.skip_confirmation!
+      user.save!
+      visit new_user_session_path
+      fill_in 'メールアドレス', with: 'test@test.com'
+      fill_in 'パスワード', with: 'test@test.com'
+      click_button 'ログイン'
+    end
+    context '一覧ページを見た場合' do
+      it '取組中の解決案が表示される' do
+        visit weekly_plans_path
+        expect(page).to have_content "取組中のテスト解決案"
+      end
+    end
+    context '一覧ページを見た場合' do
+      it '保留中の解決案は表示されない' do
+        visit weekly_plans_path
+        expect(page).not_to have_content "保留中のテスト解決案"
+      end
+    end
+  end
+end
