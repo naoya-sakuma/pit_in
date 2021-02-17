297cfd8cc4de39923a1d43d3f281160410b44699
naoyasakuma <4869ns@gmail.com>
2021-02-17 23:44:44 +0900

    [clean]user関連のテストコードを整理

--- spec/system/user/login_spec.rb
+++ spec/system/user/login_spec.rb
@@ -1,1 +1,32 @@
-# rspec spec/system/login_spec.rb
+# bundle exec rspec spec/system/user/login_spec.rb
+
+require 'rails_helper'
+RSpec.describe 'ユーザー機能', type: :system do
+  describe 'ログイン機能' do
+    context 'メールアドレスでログインした場合' do
+      it 'マイページにアクセスする' do
+        @user = FactoryBot.create(:user)
+        #@user.skip_confirmation!
+        @user.save!
+        visit new_user_session_path
+        fill_in 'メールアドレス', with: 'test@test.com'
+        fill_in 'パスワード', with: 'test@test.com'
+        click_button 'ログイン'
+        expect(page).to have_content 'テストユーザー'
+      end
+    end
+    context 'ログアウトを押した場合' do
+      it 'アカウント登録ページに戻る' do
+        @user = FactoryBot.create(:user)
+        #@user.skip_confirmation!
+        @user.save!
+        visit new_user_session_path
+        fill_in 'メールアドレス', with: 'test@test.com'
+        fill_in 'パスワード', with: 'test@test.com'
+        click_button 'ログイン'
+        click_on 'ログアウト'
+        expect(page).to have_content 'アカウント登録'
+      end
+    end
+  end
+end
--- spec/system/user/registration_spec.rb
+++ spec/system/user/registration_spec.rb
@@ -1,1 +1,26 @@
-# rspec spec/system/registration_spec.rb
+# bundle exec rspec spec/system/user/registration_spec.rb
+
+require 'rails_helper'
+RSpec.describe 'ユーザー機能', type: :system do
+  describe 'メールアドレスによるユーザー新規登録' do
+    context 'メールアドレスを入力せずに登録した場合' do
+      it 'エラーが発生し、メールアドレスを入力してくださいと表示される' do
+        visit root_path
+        fill_in '名前', with: 'テストユーザー'
+        fill_in 'パスワード', with: 'test@test.com'
+        fill_in '確認用パスワード', with: 'test@test.com'
+        click_on 'アカウント登録'
+        expect(page).to have_content 'メールアドレスを入力してください'
+      end
+    end
+    context 'パスワードを入力せずに登録した場合' do
+      it 'エラーが発生し、パスワードを入力してくださいと表示される' do
+        visit root_path
+        fill_in '名前', with: 'テストユーザー'
+        fill_in 'メールアドレス', with: 'test@test.com'
+        click_on 'アカウント登録'
+        expect(page).to have_content 'パスワードを入力してください'
+      end
+    end
+  end
+end
