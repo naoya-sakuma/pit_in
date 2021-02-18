85b0bd149b3c9f31ffa41dda1bd1024c464fff96
naoyasakuma <4869ns@gmail.com>
2021-02-18 12:41:41 +0900

    [add]communityモデルのテストを追加

--- spec/models/community_spec.rb
+++ spec/models/community_spec.rb
@@ -1,5 +1,13 @@
-require 'rails_helper'
+# bundle exec rspec spec/models/community_spec.rb
 
+require 'rails_helper'
 RSpec.describe Community, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションのテスト' do
+    context 'タイトルが空の場合' do
+      it 'バリデーションにひっかる' do
+        community = Community.new(title: '')
+        expect(community).not_to be_valid
+      end
+    end
+  end
 end
