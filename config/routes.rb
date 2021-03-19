d70b12e4aab2b59dbdb70ebd41c6f6ba71135d21
naoyasakuma <4869ns@gmail.com>
2021-03-19 16:26:30 +0900

    [change]ログイン中のroot pathを変更

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
     authenticated :user do
-      root to: 'users#show'
+      root to: 'goals#index'
     end
     unauthenticated :user do
       root to: 'devise/registrations#new'
