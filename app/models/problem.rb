7c66cdcb97c495ebf82ab07236dd73e149da06c4
naoyasakuma <4869ns@gmail.com>
2021-03-26 23:01:10 +0900

    [clean]modelのコードを整理

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,6 +1,7 @@
 class Problem < ApplicationRecord
   belongs_to :goal, inverse_of: :problems
-  has_many :solutions, inverse_of: :problem, dependent: :destroy
+  #has_many :solutions, inverse_of: :problem, dependent: :destroy
+  has_many :solutions, dependent: :destroy
   accepts_nested_attributes_for :solutions, allow_destroy: true
   validates :title, presence: true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :solution, inverse_of: :tasks
-  has_many :steps, inverse_of: :task, dependent: :destroy
+  #has_many :steps, inverse_of: :task, dependent: :destroy
+  has_many :steps, dependent: :destroy
   accepts_nested_attributes_for :steps, allow_destroy: true
   validates :title, presence: true
 end
