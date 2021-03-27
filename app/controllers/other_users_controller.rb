d21ab7b5e513e69662e65be22acf61d19a35f7e8
naoyasakuma <4869ns@gmail.com>
2021-03-27 15:41:10 +0900

    [update]ユーザー詳細に作成したコミュニティを表示するように設定

--- app/controllers/other_users_controller.rb
+++ app/controllers/other_users_controller.rb
@@ -6,5 +6,6 @@ class OtherUsersController < ApplicationController
   def show
     @other_user = User.find(params[:id])
     @shared_goals = @other_user.goals.where(share: '公開')
+    @own_communities = @other_user.communities
   end
 end
