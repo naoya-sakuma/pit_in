e4dcbc24fdda78a15e7e5e4a7c023f54a7aa6b75
naoyasakuma <4869ns@gmail.com>
2021-04-03 22:23:17 +0900

    [update]ユーザーをフォローできるように設定

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,10 @@
 class RelationshipsController < ApplicationController
+  before_action :check_logged_in
+
+  respond_to? :js
   def create
+    @user = User.find(params[:relationship][:followed_id])
+    current_user.follow!(@user)
   end
 
   def destroy
