e7cdd0b7559caf680f8cb37ec461b03cf17de800
naoyasakuma <4869ns@gmail.com>
2021-04-06 01:54:48 +0900

    [update]ユーザーの検索機能を追加

--- app/controllers/other_users_controller.rb
+++ app/controllers/other_users_controller.rb
@@ -1,4 +1,6 @@
 class OtherUsersController < ApplicationController
+  before_action :set_search, only:[:index, :search]
+
   def index
     @other_users = User.all.where.not(id: current_user.id).page(params[:page]).per(10)
   end
@@ -7,5 +9,15 @@ class OtherUsersController < ApplicationController
     @other_user = User.find(params[:id])
     @shared_goals = @other_user.goals.where(share: '公開')
     @own_communities = @other_user.communities
+  end
+
+  def search
+    @searched_results_other_users = @searched_other_users.result.page(params[:page]).per(10)
+  end
+
+  private
+  def set_search
+    @other_users = User.all.where.not(id: current_user.id)
+    @searched_other_users = @other_users.ransack(params[:q])
   end
 end
