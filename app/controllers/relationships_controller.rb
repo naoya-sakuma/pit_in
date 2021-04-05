d934627ed40e7ceea5ee01acd611059c4230f911
naoyasakuma <4869ns@gmail.com>
2021-04-06 02:03:14 +0900

    [update]searchアクションを追加

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,6 @@
 class RelationshipsController < ApplicationController
   before_action :check_logged_in
+  before_action :set_search, only:[:index, :search]
 
   def index
     @following_users = current_user.following
@@ -15,4 +16,15 @@ class RelationshipsController < ApplicationController
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
   end
+
+  def search
+    @searched_results_following_users = @searched_following_users.result.page(params[:page]).per(10)
+  end
+
+  private
+  def set_search
+    @following_users = current_user.following
+    @searched_following_users = @following_users.ransack(params[:q])
+  end
+
 end
