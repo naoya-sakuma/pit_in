ef540c9a947ea4e2ee426b22b8556912434ce33a
naoyasakuma <4869ns@gmail.com>
2021-04-07 19:07:57 +0900

    [clean]不要なコードを一時的にコメント化

--- app/assets/javascripts/add_form.js
+++ app/assets/javascripts/add_form.js
@@ -1,9 +1,9 @@
-$(document).on("click", ".add", function() {
-        $(this).parent().clone(true).insertAfter($(this).parent());
-    });
-    $(document).on("click", ".del", function() {
-        var target = $(this).parent();
-        if (target.parent().children().length > 1) {
-            target.remove();
-        }
-    });
+// $(document).on("click", ".add", function() {
+//         $(this).parent().clone(true).insertAfter($(this).parent());
+//     });
+//     $(document).on("click", ".del", function() {
+//         var target = $(this).parent();
+//         if (target.parent().children().length > 1) {
+//             target.remove();
+//         }
+//     });
