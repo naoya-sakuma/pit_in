d3d4bb5f96e72ffd39603ffeb0b5ca2c0d26a3bb
naoya-sakuma <71757155+naoya-sakuma@users.noreply.github.com>
2021-04-15 11:26:17 +0900

    Revert "To Doのレイアウトを修正"

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,9 +1,6 @@
 class Goal < ApplicationRecord
   belongs_to :user
   has_many :problems, dependent: :destroy
-  has_many :solutions, through: :problems
-  has_many :tasks, through: :solutions
-  has_many :steps, through: :tasks
   accepts_nested_attributes_for :problems, allow_destroy: true
   validates :title, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: {
     registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
@@ -13,27 +13,23 @@ Rails.application.routes.draw do
       root to: 'devise/registrations#new'
     end
   end
-
+  resources :goals
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
-  resources :users, only: [:index, :show, :edit, :update]
-  resources :goals
-  resources :monthly_plans, only: [:index, :edit, :update]
-  resources :weekly_plans, only: [:index, :edit, :update]
-  resources :daily_plans, only: [:index, :edit, :update]
-  resources :today_jobs, only: [:index]
-  resources :problems, only: [:index, :edit, :update]
-  resources :directions, only: [:index]
-  resources :members, only: [:create, :destroy]
-
-  resources :shared_goals, only: [:index, :update] do
+  resources :users
+  resources :monthly_plans
+  resources :weekly_plans
+  resources :daily_plans
+  resources :today_jobs
+  resources :problems
+  resources :directions
+  resources :shared_goals do
     collection do
       get :search
     end
   end
-
   resources :communities do
     resources :comments
     collection do
@@ -41,13 +37,12 @@ Rails.application.routes.draw do
       get :management
     end
   end
-
+  resources :members, only: [:create, :destroy]
   resources :other_users, only: [:index, :show] do
     collection do
       get :search
     end
   end
-
   resources :relationships, only: [:index, :create, :destroy] do
     collection do
       get :search
